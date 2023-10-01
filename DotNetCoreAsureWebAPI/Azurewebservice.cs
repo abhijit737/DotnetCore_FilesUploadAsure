fdc485be2c7ce1afe3145946bdfbe7794d014e7f
abhijit737 <abhijitm737@gmail.com>
2023-10-01 14:05:41 +0530

    first commit

---
+++ DotNetCoreAsureWebAPI/Azurewebservice.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models; 
+namespace DotNetCoreAsureWebAPI
+{
+    public class Azurewebservice
+    {
+        BlobServiceClient _blobServiceClient;
+        BlobContainerClient _blobContainerClient;
+        string azureConnectionstring = "DefaultEndpointsProtocol=https;AccountName=abcreatedstorage;AccountKey=CHAy0N1ouhd6ZpkveMzZiCjTXgsadS1vOz7nEesmhaDoo/r5oNCQeb0VyZAv+wK+vYcAWSvsQbpp+ASt4OL/pw==;EndpointSuffix=core.windows.net";
+
+        public Azurewebservice()
+        {
+            _blobServiceClient = new BlobServiceClient(azureConnectionstring);
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient("abcontainer");
+
+        }
+
+        public async Task<List<BlobContentInfo>>UploadFiles(List<IFormFile>files)
+        {
+            var azureResponse = new List<BlobContentInfo>();
+            foreach (var file in files)
+            {
+                string fileName = file.FileName;
+                using (var memoryStream  = new MemoryStream())
+                {
+                    file.CopyTo(memoryStream);
+                    memoryStream.Position = 0;
+
+                    var client = await _blobContainerClient.UploadBlobAsync(fileName, memoryStream, default);
+                    azureResponse.Add(client);
+                }
+            };
+
+            return azureResponse;
+        }
+        public async Task<List<BlobItem>>GetUploadedBlob()
+        {
+            var items = new List<BlobItem>();
+            var UploadedFiles = _blobContainerClient.GetBlobsAsync();
+        
+           await foreach ( BlobItem file in UploadedFiles)
+            {
+                items.Add(file);
+            }
+
+            return items;
+        }
+    }
+}
---
+++ DotNetCoreAsureWebAPI/Controllers/AsureBlobController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreAsureWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AsureBlobController : ControllerBase
+    {
+
+        public AzureBlobService _service;
+        public AsureBlobController(AzureBlobService _azservice)
+        {
+            _service = _azservice;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(List <IFormFile>files)
+        {
+            var response = await _service.UploadFiles(files);
+            return Ok(response);
+        }
+
+        [HttpGet("BlobList")]
+        public async Task<IActionResult>GetBlobList()
+        {
+            var response =  await _service.GetUploadedBlob();
+            return Ok(response);
+        }
+
+    }
+}
